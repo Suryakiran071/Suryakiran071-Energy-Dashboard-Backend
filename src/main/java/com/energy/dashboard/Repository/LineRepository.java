a172442e9d1a12ba88574b0f28051d03049a2d27
Yasala <2487551@cognizant.com>
2026-05-05 11:45:58 +0530

    Decline button, password validation, time zone normalization

--- src/main/java/com/energy/dashboard/Repository/LineRepository.java
+++
@@ -1,9 +0,0 @@
-package com.energy.dashboard.Repository;
-
-import com.energy.dashboard.Model.Line;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LineRepository extends JpaRepository<Line,Long> {
-}
--- src/main/java/com/energy/dashboard/Repository/MeterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.energy.dashboard.Repository;
-
-import com.energy.dashboard.Model.Meter;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MeterRepository extends JpaRepository<Meter,Long> {
-}
